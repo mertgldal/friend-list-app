710b7129ba6dce84e81a6815b70a15662f8f5d6e
mertgldal <mertguldal@outlook.com>
2023-07-10 11:24:38 -0700

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  #root 'home#index'
+  root 'friends#index'
+
+  get "home/about"
+  get "home/deneme"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
